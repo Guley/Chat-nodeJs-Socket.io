c3737ad7e6c2525a58809d5ae0cc69fc81e5d9b8
Gulshan <gul2787@gmail.com>
2019-08-02 12:39:23 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,8 @@
 
 var socket = io.connect('http://localhost:3000');
  var logoffTimer;
+ var userList = [];
+
 //Query Dom
 
 var message = document.getElementById('message');
@@ -9,40 +11,59 @@ var message = document.getElementById('message');
     btn = document.getElementById('send');
     output = document.getElementById('output');
     feedback = document.getElementById('feedback');
+    clients = document.getElementById('clients');
+    
 //emit events
 var logoffTimer;
+var gettingOutTimer;
+
 btn.addEventListener('click',function(){
+    var rand = Math.floor((Math.random() * 1000) + 1);
+    this.handle = (handle.value == '' )?'Anonymous-'+rand:handle.value;
+
+   
+    if(userList.indexOf(this.handle) === -1) {
+        userList.push(this.handle)   
+    } 
+
+    if(message.value == ''){
+        alert('Please type your message')
+        return;
+    }
+    handle.disabled = "disabled";
     socket.emit('chat',{
         'message':message.value,
-        'handle':handle.value
+        'handle':this.handle
     });
+    message.value = '';
+    handle.value =  this.handle;
 })
 message.addEventListener('keypress',function(){
+    this.handle = (handle.value == '' )?'Anonymous-'+rand:handle.value;
     clearTimeout(logoffTimer);
     logoffTimer = setTimeout(function(){
-        socket.emit("logoff", { reason: "Logged off due to inactivity" });
+        socket.emit("logoff", {});
     }, 60 * 30);
-    socket.emit('typing',handle.value)
+    console.log(this.handle)
+    socket.emit('typing',this.handle)
 
 })
  
 //listen on event
-
 socket.on('chat',function(data){
     feedback.innerHTML = "";
     output.innerHTML += '<p><stong>'+data.handle+'</strong>: '+data.message+'</p>';
 })
-
 socket.on('typing',function(data){
-    
     feedback.innerHTML = '<p><em>'+data+' is typing....</em></p>';
-   
 })
 socket.on('logoff',function(data){
     feedback.innerHTML = "";
 })
-
-
-
-
-
+socket.on('user-left',function(data){
+    feedback.innerHTML = '<p><em>'+data+' is left from chat..</em></p>';
+    clearTimeout(gettingOutTimer);
+    gettingOutTimer = setTimeout(function(){
+        socket.emit("logoff", {});
+    }, 60 * 30);
+})
